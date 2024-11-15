707c3fe5a51ddb59dd2dee0982c9b77988566f6b
Gandalf <quentin.pladeau@gmail.com>
2024-11-15 16:39:35 +0100

    [+] Nouvelle cam en fontion de la pos sur la map

--- groups.py
+++ groups.py
@@ -4,15 +4,26 @@ class AllSprites(pygame.sprite.Group):
     def __init__(self):
         super().__init__()
         self.display_surface = pygame.display.get_surface()
-        self.offset = pygame.Vector2(0,0)
+        self.offset = pygame.Vector2(0, 0)
+        self.map_width = LONGUEUR * CASEMAP
+        self.map_height = LARGEUR * CASEMAP
 
     def draw(self, target_pos):
-        self.offset.x = - (target_pos[0] - WINDOW_WIDTH/2)
-        self.offset.y = - (target_pos[1] - WINDOW_HEIGHT/2)
+        # Calcul de l'offset pour centrer la caméra sur le joueur
+        camera_x = -(target_pos[0] - WINDOW_WIDTH / 2)
+        camera_y = -(target_pos[1] - WINDOW_HEIGHT / 2)
 
+        # Limiter la caméra pour ne pas montrer le vide
+        camera_x = min(0, max(camera_x, WINDOW_WIDTH - self.map_width))
+        camera_y = min(0, max(camera_y, WINDOW_HEIGHT - self.map_height))
+
+        self.offset.x = camera_x
+        self.offset.y = camera_y
+
+        # Dessiner les sprites
         ground_sprites = [sprite for sprite in self if hasattr(sprite, 'ground')]
         object_sprites = [sprite for sprite in self if not hasattr(sprite, 'ground')]
 
         for layer in [ground_sprites, object_sprites]:
-            for sprite in sorted(layer, key = lambda sprite: sprite.rect.centery):
+            for sprite in sorted(layer, key=lambda sprite: sprite.rect.centery):
                 self.display_surface.blit(sprite.image, sprite.rect.topleft + self.offset)
