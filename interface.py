8394e67d9a9b92294054735f601ecc1ebe247a7e
Gandalf <quentin.pladeau@gmail.com>
2024-12-15 23:37:25 +0100

    [~] base interface des element settings

---
+++ interface.py
@@ -0,0 +1,13 @@
+from settings import *
+
+class SettingsInterface(object):
+    pass
+
+class SoudInterface(object):
+    pass
+
+class BundleInterface(object):
+    pass
+
+class BookInterface(object):
+    pass
