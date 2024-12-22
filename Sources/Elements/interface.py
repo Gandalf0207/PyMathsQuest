245b8206bede51fc51a73c61b57d642724123003
Gandalf <quentin.pladeau@gmail.com>
2024-12-22 13:27:42 +0100

    [+] Commentaire : fichier interface.py, patch name

--- Sources/Elements/interface.py
+++ Sources/Elements/interface.py
@@ -1,119 +1,190 @@
 from settings import *
 
 class SettingsInterface(object):
-    def __init__(self, gestionnaire):
-        self.displaySurface = pygame.display.get_surface()
-        self.gestionnaire = gestionnaire
 
+    def __init__(self, gestionnaire: any) ->None:
+        """Méthode initialisation de l'interface de settings.
+        Input : gestionnaire = self méthode d'appel"""
+        
+        # Initialisation
+        self.gestionnaire = gestionnaire
+        
+        # Création éléments
+        self.displaySurface = pygame.display.get_surface() # surface générale
         self.interfaceSurface = pygame.Surface((WINDOW_WIDTH/2, WINDOW_HEIGHT/2),  pygame.SRCALPHA)
         self.interfaceSurface.fill("#ffffff")
 
+        # texte 
         self.font = pygame.font.Font(None, 36)
         self.titreText = "Settings"
 
 
-    def BuildInterface(self):
+    def BuildInterface(self) -> None:
+        """Méthode : Création de tout les éléments composant l'interface. Input / Output : None"""
+
+        # texte titre
         text = self.font.render(self.titreText, True, (0,0,0))
         self.interfaceSurface.blit(text, (10,10))
 
-    def CloseInterface(self):
+
+    def CloseInterface(self) -> None:
+        """Méthode de fermeture de l'interface. Input / Output : None"""
+
+        # changement des boolean de check
         self.gestionnaire.InterfaceOpen = False
         self.gestionnaire.INTERFACE_OPEN = False
 
-    def Update(self):
-        self.BuildInterface()
-        self.displaySurface.blit(self.interfaceSurface, (320,180))
 
+    def Update(self) -> None:
+        """Méthode d'update de l'interface. Input / Output : None"""
+
+        # construction d'update
+        self.BuildInterface()
+        self.displaySurface.blit(self.interfaceSurface, (320,180)) # pos topleft
 
         # Fermer l'interface avec ESC
         keys = pygame.key.get_pressed()
         if keys[pygame.K_ESCAPE]:  # Fermer avec ESC
             self.CloseInterface()
 
+
 class SoudInterface(object):
-    def __init__(self, gestionnaire):
-        self.displaySurface = pygame.display.get_surface()
+
+    def __init__(self, gestionnaire: any) ->None:
+        """Méthode initialisation de l'interface de sound.
+        Input : gestionnaire = self méthode d'appel"""
+        
+        # Initialisation
         self.gestionnaire = gestionnaire
-
+        
+        # Création éléments
+        self.displaySurface = pygame.display.get_surface() # surface générale
         self.interfaceSurface = pygame.Surface((WINDOW_WIDTH/2, WINDOW_HEIGHT/2),  pygame.SRCALPHA)
-        self.interfaceSurface.fill("#47154a")
+        self.interfaceSurface.fill("#ffffff")
 
+        # texte 
         self.font = pygame.font.Font(None, 36)
         self.titreText = "Sound"
 
 
-    def BuildInterface(self):
+    def BuildInterface(self) -> None:
+        """Méthode : Création de tout les éléments composant l'interface. Input / Output : None"""
+
+        # texte titre
         text = self.font.render(self.titreText, True, (0,0,0))
         self.interfaceSurface.blit(text, (10,10))
 
-    def CloseInterface(self):
+
+    def CloseInterface(self) -> None:
+        """Méthode de fermeture de l'interface. Input / Output : None"""
+
+        # changement des boolean de check
         self.gestionnaire.InterfaceOpen = False
         self.gestionnaire.INTERFACE_OPEN = False
 
-    def Update(self):
-        self.BuildInterface()
-        self.displaySurface.blit(self.interfaceSurface, (320,180))
 
+    def Update(self) -> None:
+        """Méthode d'update de l'interface. Input / Output : None"""
+
+        # construction d'update
+        self.BuildInterface()
+        self.displaySurface.blit(self.interfaceSurface, (320,180)) # pos topleft
 
         # Fermer l'interface avec ESC
         keys = pygame.key.get_pressed()
         if keys[pygame.K_ESCAPE]:  # Fermer avec ESC
             self.CloseInterface()
 
+
 class BundleInterface(object):
-    def __init__(self, gestionnaire):
-        self.displaySurface = pygame.display.get_surface()
+
+    def __init__(self, gestionnaire: any) ->None:
+        """Méthode initialisation de l'interface de bundle.
+        Input : gestionnaire = self méthode d'appel"""
+        
+        # Initialisation
         self.gestionnaire = gestionnaire
-
+        
+        # Création éléments
+        self.displaySurface = pygame.display.get_surface() # surface générale
         self.interfaceSurface = pygame.Surface((WINDOW_WIDTH/2, WINDOW_HEIGHT/2),  pygame.SRCALPHA)
-        self.interfaceSurface.fill("#c3c3c3")
+        self.interfaceSurface.fill("#ffffff")
 
+        # texte 
         self.font = pygame.font.Font(None, 36)
         self.titreText = "Bundle"
 
 
-    def BuildInterface(self):
+    def BuildInterface(self) -> None:
+        """Méthode : Création de tout les éléments composant l'interface. Input / Output : None"""
+
+        # texte titre
         text = self.font.render(self.titreText, True, (0,0,0))
         self.interfaceSurface.blit(text, (10,10))
 
-    def CloseInterface(self):
+
+    def CloseInterface(self) -> None:
+        """Méthode de fermeture de l'interface. Input / Output : None"""
+
+        # changement des boolean de check
         self.gestionnaire.InterfaceOpen = False
         self.gestionnaire.INTERFACE_OPEN = False
 
-    def Update(self):
-        self.BuildInterface()
-        self.displaySurface.blit(self.interfaceSurface, (320,180))
 
+    def Update(self) -> None:
+        """Méthode d'update de l'interface. Input / Output : None"""
+
+        # construction d'update
+        self.BuildInterface()
+        self.displaySurface.blit(self.interfaceSurface, (320,180)) # pos topleft
 
         # Fermer l'interface avec ESC
         keys = pygame.key.get_pressed()
         if keys[pygame.K_ESCAPE]:  # Fermer avec ESC
             self.CloseInterface()
 
+
 class BookInterface(object):
-    def __init__(self, gestionnaire):
-        self.displaySurface = pygame.display.get_surface()
+
+    def __init__(self, gestionnaire: any) ->None:
+        """Méthode initialisation de l'interface de book.
+        Input : gestionnaire = self méthode d'appel"""
+        
+        # Initialisation
         self.gestionnaire = gestionnaire
-
+        
+        # Création éléments
+        self.displaySurface = pygame.display.get_surface() # surface générale
         self.interfaceSurface = pygame.Surface((WINDOW_WIDTH/2, WINDOW_HEIGHT/2),  pygame.SRCALPHA)
-        self.interfaceSurface.fill("#000000")
+        self.interfaceSurface.fill("#ffffff")
 
+        # texte 
         self.font = pygame.font.Font(None, 36)
         self.titreText = "Book"
 
 
-    def BuildInterface(self):
+    def BuildInterface(self) -> None:
+        """Méthode : Création de tout les éléments composant l'interface. Input / Output : None"""
+
+        # texte titre
         text = self.font.render(self.titreText, True, (0,0,0))
         self.interfaceSurface.blit(text, (10,10))
 
-    def CloseInterface(self):
+
+    def CloseInterface(self) -> None:
+        """Méthode de fermeture de l'interface. Input / Output : None"""
+
+        # changement des boolean de check
         self.gestionnaire.InterfaceOpen = False
         self.gestionnaire.INTERFACE_OPEN = False
 
-    def Update(self):
-        self.BuildInterface()
-        self.displaySurface.blit(self.interfaceSurface, (320,180))
 
+    def Update(self) -> None:
+        """Méthode d'update de l'interface. Input / Output : None"""
+
+        # construction d'update
+        self.BuildInterface()
+        self.displaySurface.blit(self.interfaceSurface, (320,180)) # pos topleft
 
         # Fermer l'interface avec ESC
         keys = pygame.key.get_pressed()
@@ -121,10 +192,19 @@ class BookInterface(object):
             self.CloseInterface()
 
 
+
+
 class PNJInterface(object):
-    def __init__(self, gestionnaire):
+
+    def __init__(self, gestionnaire : any) -> None:
+        """Méthode initialisation de l'interface de discussion avec les pnj.
+        Input : gestionnaire (self class d'appel), Output : None."""
+
+        # Initialisation
         self.gestionnaire = gestionnaire
-        self.displaySurface = pygame.display.get_surface()
+        
+        # Création éléments
+        self.displaySurface = pygame.display.get_surface() # surface générale
 
         # Surface pour le fond transparent
         self.backgroundSurface = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.SRCALPHA)
@@ -134,71 +214,75 @@ class PNJInterface(object):
         self.interfaceSurface = pygame.Surface((WINDOW_WIDTH, WINDOW_HEIGHT), pygame.SRCALPHA)
         self.interfaceSurface.fill((0, 0, 0, 0))  # Transparent par défaut
 
-
         # Initialisation des polices
         self.font1 = pygame.font.Font(None, 36)
         self.font1.set_bold(True)
-
         self.font2 = pygame.font.Font(None, 36)
 
         # Position du PNJ
         self.pnj_position = (100, 100)  # Position du PNJ (à gauche)
 
-        # Texte pour le PNJ
+        # Variables pour le texte du pnj
         self.pnj_text = None
-
-        # Variables pour l'effet de texte
         self.pnj_displayed_text = ""  # Texte affiché du PNJ
         self.pnj_index = 0  # Index pour le texte du PNJ
         self.compteurDialogue = 1
         self.nombreDialogue = len(self.gestionnaire.allDialogues[f"Niveau{self.gestionnaire.niveau}"][self.gestionnaire.pnjActuel]["Principal"]) if not self.gestionnaire.pnjObj.discussion else len(self.gestionnaire.allDialogues[f"Niveau{self.gestionnaire.niveau}"][self.gestionnaire.pnjActuel]["Alternatif"])
 
+        # timer click skip
         self.last_click_time = 0
         self.click_delay = 500    
 
-
+        # chargement des éléments autre
         self.loadPNG()
         self.loadText()
 
-    def loadText(self):
+
+    def loadText(self) -> None:
+        """Méthode de chargement des dialogues en fontion du pnj et du niveau et de l'avancement de discussion. 
+        Input / Output : None"""
+
+        # variables de base
         self.pnj_displayed_text = ""
         self.pnj_index = 0
 
-        print(self.gestionnaire.pnjObj.discussion, "on a blablaa")
-
+        # check déjà discuter avec
         if not self.gestionnaire.pnjObj.discussion:
+            # chargement du texte
             if self.compteurDialogue <= self.nombreDialogue:
                 self.pnj_text = self.gestionnaire.allDialogues[f"Niveau{self.gestionnaire.niveau}"][self.gestionnaire.pnjActuel]["Principal"][f"Dialogue{self.compteurDialogue}"]
-                self.compteurDialogue += 1
-                print(self.pnj_text)
+                self.compteurDialogue += 1 # passage au dialogue suivant
             else:
-                self.gestionnaire.Vu()
-                self.CloseInterface()
+                self.gestionnaire.Vu() # bool de check passage
+                self.CloseInterface() # fermeture interface
         else:
             if self.compteurDialogue <= self.nombreDialogue:
                 self.pnj_text = self.gestionnaire.allDialogues[f"Niveau{self.gestionnaire.niveau}"][self.gestionnaire.pnjActuel]["Alternatif"][f"Dialogue{self.compteurDialogue}"]
-                self.compteurDialogue += 1
-                print(self.pnj_text)
+                self.compteurDialogue += 1 # passage au dialogue suivant
             else:
-                self.CloseInterface()
+                self.CloseInterface() # fermeture interface
 
-    def loadPNG(self):
-        self.pnjImage = pygame.image.load(join("Images", "PNJ", "Discussion", f"Grand{self.gestionnaire.pnjActuel}.png"))
-        self.playerImage = pygame.image.load(join("Images", "Player", "Discussion", "GrandPlayer.png"))
 
-    def BuildInterface(self):
+    def loadPNG(self) -> None:
+        """Méthode de chargement des images.
+        Input / Output : None"""
+        
+        self.pnjImage = pygame.image.load(join("Images", "PNJ", "Discussion", f"Grand{self.gestionnaire.pnjActuel}.png")).convert_alpha()
+        self.playerImage = pygame.image.load(join("Images", "Player", "Discussion", "GrandPlayer.png")).convert_alpha()
+
+
+    def BuildInterface(self) -> None:
+        """Méthode de création des éléments de l'interface de discussion avec le pnj. 
+        Input / Output : None"""
+
         self.interfaceSurface.fill((0, 0, 0, 0))  # Réinitialiser la surface avec une transparence complète
-
 
         # load element png
         self.interfaceSurface.blit(self.pnjImage, (50,360))
         self.interfaceSurface.blit(self.playerImage, (WINDOW_WIDTH-178, 360))
 
-        # load nom pnj
-        match self.gestionnaire.pnjActuel:
-            case "PNJ1":
-                self.pnjName = "NameToDefined" 
-
+        # load nom pnj + creation text du nom
+        self.pnjName = self.gestionnaire.allDialogues[f"Niveau{self.gestionnaire.niveau}"][self.gestionnaire.pnjActuel]["Nom"]
         pnjName = self.font1.render(self.pnjName, True, (255,255,255))
         self.interfaceSurface.blit(pnjName, (200, 400))
 
@@ -246,11 +330,20 @@ class PNJInterface(object):
             line_surface = self.font2.render(line, True, (255, 255, 255))
             self.interfaceSurface.blit(line_surface, (200, y_offset + i * line_height))
 
-    def CloseInterface(self):
+
+    def CloseInterface(self) -> None:
+        """Méthode de fermeture de l'interface de discussion avec le pnj.
+        Input / Output : None"""
+
+        # Mise à jour des bool de check
         self.gestionnaire.openInterface = False
         self.gestionnaire.INTERFACE_OPEN = False
 
-    def Update(self, event):
+
+    def Update(self, event : any) -> None:
+        """Méthode d'update de l'interface de discussion.
+        Input : event = (element pygame), Output : None"""
+
         # Dessiner le fond transparent
         self.displaySurface.blit(self.backgroundSurface, (0, 0))
 
@@ -263,7 +356,7 @@ class PNJInterface(object):
         if keys[pygame.K_ESCAPE]:  # Fermer avec ESC
             self.CloseInterface()
 
-        # Vérifie si l'événement est un clic de souris
+        # Vérifie si l'événement est un clic de souris (bouton skip)
         if event.type == pygame.MOUSEBUTTONDOWN and event.button ==1:
             current_time = pygame.time.get_ticks()
             if current_time - self.last_click_time > self.click_delay:
@@ -271,5 +364,5 @@ class PNJInterface(object):
 
                 # Vérifiez si le clic est dans le rectangle du bouton
                 if self.btnRectSkip.collidepoint(event.pos):
-                    self.loadText()
-                    self.BuildInterface()
+                    self.loadText() # pasage au dialogue suivant
+                    self.BuildInterface() # build des éléments
--- Sources/Personnages/creationDialogues.py
+++ Sources/Personnages/creationDialogues.py
@@ -1,117 +1,109 @@
 from settings import *
 
 Dialogues = {
-    "Niveau0" : {
-        "PNJ1" : {
-            "Principal" : {
-                "Dialogue1" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                "Dialogue2" : "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
-                "Dialogue3" : "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."    
+    "Niveau0": {
+        "PNJ1": {
+            "Nom" : "Abrakadabra",
+            "Principal": {
+                "Dialogue1": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "Dialogue2": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+                "Dialogue3": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             },
-
-            "Alternatif" : {
-                "Dialogue1" : "deja vu"
+            "Alternatif": {
+                "Dialogue1": "deja vu"
             }
         },
-
-        "PNJ2" : {
-            "Principal" : {
-                "Dialogue1" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                "Dialogue2" : "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
-                "Dialogue3" : "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."    
+        "PNJ2": {
+            "Nom" : "Abrakadabra",
+            "Principal": {
+                "Dialogue1": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "Dialogue2": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+                "Dialogue3": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             },
-
-            "Alternatif" : {
-                "Dialogue1" : "deja vu"
-            }           
+            "Alternatif": {
+                "Dialogue1": "deja vu"
+            }
         },
-
-        "PNJ3" : {
-            "Principal" : {
-                "Dialogue1" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                "Dialogue2" : "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
-                "Dialogue3" : "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."    
+        "PNJ3": {
+            "Nom" : "Abrakadabra",
+            "Principal": {
+                "Dialogue1": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "Dialogue2": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+                "Dialogue3": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             },
-
-            "Alternatif" : {
-                "Dialogue1" : "deja vu"
-            }           
+            "Alternatif": {
+                "Dialogue1": "deja vu"
+            }
         }
     },
-
-    "Niveau1" : {
-        "PNJ1" : {
-            "Principal" : {
-                "Dialogue1" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                "Dialogue2" : "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
-                "Dialogue3" : "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."    
+    "Niveau1": {
+        "PNJ1": {
+            "Nom" : "Abrakadabra",
+            "Principal": {
+                "Dialogue1": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "Dialogue2": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+                "Dialogue3": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             },
-
-            "Alternatif" : {
-                "Dialogue1" : "deja vu"
+            "Alternatif": {
+                "Dialogue1": "deja vu"
             }
         },
-
-        "PNJ2" : {
-            "Principal" : {
-                "Dialogue1" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                "Dialogue2" : "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
-                "Dialogue3" : "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."    
+        "PNJ2": {
+            "Nom" : "Abrakadabra",
+            "Principal": {
+                "Dialogue1": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "Dialogue2": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+                "Dialogue3": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             },
-
-            "Alternatif" : {
-                "Dialogue1" : "deja vu"
-            }           
+            "Alternatif": {
+                "Dialogue1": "deja vu"
+            }
         },
-
-        "PNJ3" : {
-            "Principal" : {
-                "Dialogue1" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                "Dialogue2" : "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
-                "Dialogue3" : "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."    
+        "PNJ3": {
+            "Nom" : "Abrakadabra",
+            "Principal": {
+                "Dialogue1": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "Dialogue2": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+                "Dialogue3": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             },
-
-            "Alternatif" : {
-                "Dialogue1" : "deja vu"
-            }           
+            "Alternatif": {
+                "Dialogue1": "deja vu"
+            }
         }
     },
-
-    "Niveau3" : {
-        "PNJ1" : {
-            "Principal" : {
-                "Dialogue1" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                "Dialogue2" : "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
-                "Dialogue3" : "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."    
+    "Niveau3": {
+        "PNJ1": {
+            "Nom" : "Abrakadabra",
+            "Principal": {
+                "Dialogue1": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "Dialogue2": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+                "Dialogue3": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             },
-
-            "Alternatif" : {
-                "Dialogue1" : "deja vu"
+            "Alternatif": {
+                "Dialogue1": "deja vu"
             }
         },
-
-        "PNJ2" : {
-            "Principal" : {
-                "Dialogue1" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                "Dialogue2" : "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
-                "Dialogue3" : "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."    
+        "PNJ2": {
+            "Nom" : "Abrakadabra",
+            "Principal": {
+                "Dialogue1": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "Dialogue2": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+                "Dialogue3": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             },
-
-            "Alternatif" : {
-                "Dialogue1" : "deja vu"
-            }           
+            "Alternatif": {
+                "Dialogue1": "deja vu"
+            }
         },
-
-        "PNJ3" : {
-            "Principal" : {
-                "Dialogue1" : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-                "Dialogue2" : "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
-                "Dialogue3" : "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."    
+        "PNJ3": {
+            "Nom" : "Abrakadabra",
+            "Principal": {
+                "Dialogue1": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                "Dialogue2": "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur",
+                "Dialogue3": "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             },
-
-            "Alternatif" : {
-                "Dialogue1" : "deja vu"
-            }           
+            "Alternatif": {
+                "Dialogue1": "deja vu"
+            }
         }
     }
 }
