31954891930daa4eb803cc8b05bbc1003dd0de7e
Gandalf <quentin.pladeau@gmail.com>
2025-01-02 01:11:52 +0100

    [+] Ajout inventaire affichage interface

--- settings.py
+++ settings.py
@@ -29,7 +29,20 @@ TEXTE = {
     "Elements" : None
 }
 
-INVENTORY = []
+INVENTORY = {
+    "Planks" : 0,
+    "OldAxe" : 0,
+    "Pickaxe" : 0,
+    "a" : 0,
+    "b" : 0,
+    "c" : 0,
+    "d" : 0,
+    "e" : 0,
+    "f" : 0,
+    "g" : 0,
+    "h" : 0,
+    "i" : 0,
+}
 
 PNJ = {
     "PNJ1" : False,
