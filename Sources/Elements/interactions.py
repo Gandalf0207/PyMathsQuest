3adab1d267b1f864d138de7180e12ef3dccca4e4
Gandalf <quentin.pladeau@gmail.com>
2025-01-12 00:58:58 +0100

    [+] Ajout commentaire fichier interaction

--- Sources/Elements/interactions.py
+++ Sources/Elements/interactions.py
@@ -2,15 +2,18 @@ from settings import *
 
 class Interactions(object):
 
-    def __init__(self, gestionnaire):
+    def __init__(self, gestionnaire : any) -> None:
+        """Méthode initialisation de la gestion avec les interactions (pont, rocher... ) sauf pnj
+        Input : gestionanire (self parent)
+        Output : None"""
 
-        self.player = None
         self.displaySurface = pygame.display.get_surface() # surface générale
         self.gestionnaire = gestionnaire
+        self.player = None
 
+        # element interactions
         self.camera_offset = [0,0]
         self.npc_screen_pos = [0,0]
-
         self.distanceMax = 100 # depuis centre element pont
 
         # infos de la map 
@@ -25,56 +28,61 @@ class Interactions(object):
         self.interactionGroup = None
 
 
-    def Interagir(self):
-        if self.Isclose():
+    def Interagir(self) -> None:
+        """Méthode de calcul d'interaction entre chaque element en fonction des niveaux
+        Input / Output : None"""
+
+        if self.Isclose(): # si proximité
+
+            # animation 
             self.gestionnaire.fondu_au_noir()
 
+            # niveau 0
             if INFOS["Niveau"] == 0:
 
-                # action si c'est un pont
+                # action si c'est un pont 
                 if self.ObjectId == "pont1" or self.ObjectId == "pont2" :
-                    if not self.Obj.InfoExo or INFOS["ExoPasse"]:
-                        
-                        # on change l'etat du bloc car l'exo est réussit
-                        if INFOS["ExoPasse"]:
-                            self.Obj.InfoExo = False
-                            INFOS["ExoPasse"] = False
-                            INFOS["Exo"] = False
-                            INFOS["ChangementNiveau"] = True
 
+                    if not self.Obj.InfoExo: # si c'est pas le pont d'exo
 
+                        # text animation
                         self.gestionnaire.textScreen(TEXTE["Elements"][f"Niveau{INFOS["Niveau"]}"]["TraverserPont"])
 
+                        # deplacement player
                         if self.player.rect.x < self.coordObjActuel[0]:
                             self.player.rect.x += CASEMAP*2
-                            STATE_HELP_INFOS[0] = "SeePNJ"
+                            STATE_HELP_INFOS[0] = "SeePNJ" # update tips player
                         else:
                             self.player.rect.x -= CASEMAP*2
-                            STATE_HELP_INFOS[0] = "CrossBridge"
+                            STATE_HELP_INFOS[0] = "CrossBridge" # update tips player
 
                         self.player.hitbox_rect.center = self.player.rect.center
                         
+                        # fin animation
                         self.gestionnaire.ouverture_du_noir(self.player.rect.center)
 
-                    else:
-                        INFOS["Exo"] = True
-                        self.gestionnaire.textScreen(TEXTE["Elements"][f"Niveau{INFOS["Niveau"]}"]["MakeExo"])
+                    else: # si c'est le pont exo
+                        INFOS["Exo"] = True # lancement exo dans main (changement variable)
+                        self.gestionnaire.textScreen(TEXTE["Elements"][f"Niveau{INFOS["Niveau"]}"]["MakeExo"]) # text animation
                     
                     
                     
-                # action si c'est le rocher de fin
+                # si c'est le rocher qui est destructible
                 elif self.ObjectId == "ExitRock":
-                    self.gestionnaire.textScreen(TEXTE["Elements"][f"Niveau{INFOS["Niveau"]}"]["BreakRock"])
-                    self.Obj.kill()
+                    self.gestionnaire.textScreen(TEXTE["Elements"][f"Niveau{INFOS["Niveau"]}"]["BreakRock"]) # text 
+                    self.Obj.kill() # destrcution rocher
 
-                    self.gestionnaire.ouverture_du_noir(self.player.rect.center)
-
-
+                    self.gestionnaire.ouverture_du_noir(self.player.rect.center) # fin animation
 
 
 
-    def Isclose(self):
-        for Object in self.interactionGroup:
+    def Isclose(self) -> bool :
+        """Méthode calcul de proximité entre le player et les obj interactions
+        Input : None
+        Ouput : bool"""
+
+        for Object in self.interactionGroup: # parcours all obj 
+
             coordsObj = (Object.pos[0] + CASEMAP // 2, Object.pos[1] + CASEMAP // 2) # top left coords -> center coords
             playerPos = self.player.rect.center # center player
             
@@ -92,14 +100,14 @@ class Interactions(object):
 
             if distance <= self.distanceMax:
 
-                # valeur importante du pnj à proximité
+                # valeur importante de l'obj interactions
                 self.Obj = Object
                 self.coordObjActuel = Object.pos
                 self.ObjectId = Object.id
 
                 # Dessiner la boîte d'indication "Press E"
                 font = pygame.font.Font(None, 24)
-                text_surface = font.render("Press E", True, (255, 255, 255))
+                text_surface = font.render(TEXTE["Elements"]["Niveau0"]["CanBuildBridge"], True, (255, 255, 255))
                 text_rect = text_surface.get_rect()
                 text_rect.topleft = (self.npc_screen_pos[0] - 20, self.npc_screen_pos[1] - 40)
                 
@@ -111,13 +119,18 @@ class Interactions(object):
                 # Affiche le texte
                 self.displaySurface.blit(text_surface, text_rect)
 
-                # pnj à proximité
+                # element à proximité
                 return True
             
-        # pas de pnj à proximité
+        # pas d'element à proximité
         return False
 
-    def Update(self, player, interactionGroup):
-        self.interactionGroup = interactionGroup
-        self.player = player
-        self.Isclose()
+    def Update(self, player : tuple, interactionGroup : any) -> None:
+        """Méthode update de toutes les interaction possibles (hors pnj)
+        Input : player pos : tuple
+                interactionGroup : element pygame
+        Output : None"""
+
+        self.interactionGroup = interactionGroup # maj groupe
+        self.player = player 
+        self.Isclose() # affichage texte d'interaction si proximité
