cabb7124b55d0fa05a87e4c458abaf2d9817437f
Gandalf <quentin.pladeau@gmail.com>
2025-03-15 15:09:16 +0100

    [+] Ajout de tout les dialogues des personnages

--- SourcesFichiers/Ressources/Texte/dialogues.py
+++ SourcesFichiers/Ressources/Texte/dialogues.py
@@ -36,13 +36,13 @@ DialoguesFr = {
 
     "NiveauMedievale" : {
         "PNJ1": {
-            "Nom" : "Bûcheron",
+            "Nom" : "Fermier",
             "Principal": {
-                "Dialogue1": "Bonjour toué ! Dis donc tu m’as l’air de frapper ton waterloo toi ! Mais lâche pas la patate je vais t’aider, cet arbre va pas te bloquer le chemin longtemps tabarnak !",
-                "Dialogue2": "Aussi prends c’te babiole ça pourrait t’aider t’sais, il y a écrit un charabia dessus j’te la laisse.",
+                "Dialogue1": "Hé toi, t’aurais-tu pas vu mon âne ? Il s’est enfuit pas plus tard qu’hier, et j’le r’trouve plus. Tu l’as vu, dis-tu ?",
+                "Dialogue2": "Tu me sauves dis donc, j’te passe ce truc en remerciement, t’façon j’sais po lire et puis tu devrais t’munir d’une ou deux bûches sait-on jamais.",
             },
             "Alternatif": {
-                "Dialogue1": "Alors, fonce l’ami n’ai pas peur de l’eau sous le tronc elle te sautera pas dessus !"
+                "Dialogue1": "Merci encore copaing, je vais aller chercher cette tête de mule prochainement."
             }
         },
         "PNJ2": {
@@ -58,161 +58,164 @@ DialoguesFr = {
             }
         },
         "PNJ3": {
-            "Nom" : "Âne fuyard",
+            "Nom" : "Minerva",
             "Principal": {
-                "Dialogue1": "(Il semble que cet âne se soit enfui, il y a probablement une ferme à proximité. Il a laissé tomber deux objets, on dirait un parchemin et une pioche.)",
-                "Dialogue2": "2(Il semblee cet âne se soit enui, il y a probablement une ferme à proximité. Il a laissé tomber deux objets, on dirait un parchemin et une pioche.)",
-                "Dialogue3": "3(Il semble que cet âne se s enfui, il y a probablement une fme à proximité. Il a laissé toer deux objets, on dirait un parchemin et une pioche.)",
-                "Dialogue4": "4(Il semble que cet  se soit enfui, il y a probablement une ferme à proximité. Il a laissé tomber deux objets, on diraitn parchemin et une pioche.)",
+                "Dialogue1": "Bonjour citoyen, je suis la garde Minerva ! Si vous souhaitez passer il vous faudra répondre à une énigme particulièrement compliquée. Voilà l'énigme, l’arrondi de pi est-il bien 3.14 ? ",
+                "Dialogue2": "Bien joué vous avez réussi l’énigme vous pouvez passer. Je vous souhaite une bonne journée.",
             },
             "Alternatif": {
-                "Dialogue1": "(Cet âne n’a pas l’air de vouloir rentrer chez lui)"
+                "Dialogue1": "Bien joué vous avez réussi l’énigme vous pouvez passer. Je vous souhaite une bonne journée."
             }
         },
         "PNJ4": {
-            "Nom" : "Âne fuyard",
+            "Nom" : "Roi",
             "Principal": {
-                "Dialogue1": "1(Il semble que cet âne se soit enfui, il y a probabment une ere à proté. l  laissé tber deuobjets, dirait un paremin et une pioche.)",},
+                "Dialogue1": "AHAHAH, J’ai trouvé un moyen de voyager à travers les dimensions et de tout conquérir !!! ",
+                "Dialogue2" : "Essayez de m'arrêter si vous l’osez pauvre aventurier de pacotille !"
+                },
             "Alternatif": {
-                "Dialogue1": "(Cet âne n’a pas l’air de vouloir rentrer chez lui)"
+                "Dialogue1": "..."
             }
         },
     },
 
     "NiveauBaseFuturiste" : {
         "PNJ1": {
-            "Nom" : "Bûcheron",
+            "Nom" : "Michael",
             "Principal": {
-                "Dialogue1": "Bonjour toué ! Dis donc tu m’as l’air de frapper ton waterloo toi ! Mais lâche pas la patate je vais t’aider, cet arbre va pas te bloquer le chemin longtemps tabarnak !",
-                "Dialogue2": "Aussi prends c’te babiole ça pourrait t’aider t’sais, il y a écrit un charabia dessus j’te la laisse.",
+                "Dialogue1": "Oulah d'où est-ce que tu viens toi ?",
+                "Dialogue2": "Ton portail à fait sauter le courant dans toute la base, il va falloir le rallumer ou tu risques pas de rentrer chez toi.",
             },
             "Alternatif": {
-                "Dialogue1": "Alors, fonce l’ami n’ai pas peur de l’eau sous le tronc elle te sautera pas dessus !"
-            }
+                "Dialogue1": "J’irais bien allumer le réacteur mais sans courant les portes ne s’ouvriront pas il faudra trouver un autre moyen.",
+                "Dialogie2": "Tenez je crois que ceci est tombé avec vous.",
+            },
         },
         "PNJ2": {
-            "Nom" : "Voyageur curieux",
+            "Nom" : "Ingénieure",
             "Principal": {
-                "Dialogue1": "Salut l’ami si tu souhaites traverser l’Anduin il te faudra créer un pont je n’ai rien d’autre que ces planches alors je vais te les donner et j'espère que tu en feras bon usage.",
-                "Dialogue2": "J’ai même un manuel mais je ne sais pas s’il te sera utile.",
+                "Dialogue1": "Allo ! Il y a quelqu’un ? Je ne vois plus rien quelqu’un peut m’aider ?",
+                "Dialogue2": "Il faut redémarrer le réacteur pour récupérer le courant mais je ne trouve pas le bouton.",
+                "Dialogue3": "Attention à ne pas abuser du bouton ça pourrait être dangereux.",
             },
             "Alternatif": {
-                "Dialogue1": "Alors que trouve-t-on de l’autre côté ? Tu me raconteras une fois que tu l’auras vu."
+                "Dialogue1": "T’as trouvé le bouton ? Je viens de me rendre compte que je suis aveugle et qu’en réalité ça ne change rien pour moi."
             }
         },
         "PNJ3": {
-            "Nom" : "Âne fuyard",
+            "Nom" : "Han",
             "Principal": {
-                "Dialogue1": "(Il semble que cet âne se soit enfui, il y a probablement une ferme à proximité. Il a laissé tomber deux objets, on dirait un parchemin et une pioche.)",
-                "Dialogue2": "2(Il semblee cet âne se soit enui, il y a probablement une ferme à proximité. Il a laissé tomber deux objets, on dirait un parchemin et une pioche.)",
-                "Dialogue3": "3(Il semble que cet âne se s enfui, il y a probablement une fme à proximité. Il a laissé toer deux objets, on dirait un parchemin et une pioche.)",
-                "Dialogue4": "4(Il semble que cet  se soit enfui, il y a probablement une ferme à proximité. Il a laissé tomber deux objets, on diraitn parchemin et une pioche.)",
+                "Dialogue1": "T’as besoin d’un pilote p’tit gars ? Je peux faire le boulot mais va falloir raquer.",
+                "Dialogue2": "Et si t’as pas d’argent on peut faire un peu de troc.",
+                "Dialogue3": "Tu veux m’acheter avec une pelle, une hache et une pioche ??? Ça tombe bien je comptais refaire mon jardin. Tiens je te passe ce carnet de photo de mes fleurs.",
+                "Dialogue4": "(Il se trouve qu’Han s’est trompé et a donné une feuille de math à la place)",
             },
             "Alternatif": {
-                "Dialogue1": "(Cet âne n’a pas l’air de vouloir rentrer chez lui)"
+                "Dialogue1": "..."
             }
         },
         "PNJ4": {
-            "Nom" : "Âne fuyard",
+            "Nom" : "Didier ",
             "Principal": {
-                "Dialogue1": "1(Il semble que cet âne se soit enfui, il y a probabment une ere à proté. l  laissé tber deuobjets, dirait un paremin et une pioche.)",
-                "Dialogue2": "2(Il semble que cet âne se soit enfui, il y a probabment une ere à proté. l  laissé tber deuobjets, dirait un paremin et une pioche.)",
-                "Dialogue3": "3(Il semble que cet âne se soit enfui, il y a probabment une ere à proté. l  laissé tber deuobjets, dirait un paremin et une pioche.)",
-                "Dialogue4": "4(Il semble que cet âne se soit enfui, il y a probabment une ere à proté. l  laissé tber deuobjets, dirait un paremin et une pioche.)",
+                "Dialogue1": "Beh dis donc si tu veux partir d’ici il te faudra un vaisseau mon cochon !",
+                "Dialogue2": "Et pis même si t’en as un, il lui faudra bien du jus pour décoller !",
+                "Dialogue3": "Va donc voir pour pomper un peu de carburant dans c’te salle on est pas encore passé à l’électrique.",
                 },
 
 
             "Alternatif": {
-                "Dialogue1": "(Cet âne n’a pas l’air de vouloir rentrer chez lui)"
+                "Dialogue1": "Alors dépêche toi avant que les prix augmentent ! Je sais même pas pourquoi j’dis ça on la paie pas ici."
             }
         },
         "PNJ5": {
-            "Nom" : "Âne fuyard",
+            "Nom" : "Han",
             "Principal": {
-                "Dialogue1": "1(Il semble que cet âne se soit enfui, il y a probabment une ere à proté. l  laissé tber deuobjets, dirait un paremin et une pioche.)",
-                "Dialogue2": "2(Il semble que cet âne se soit enfui, il y a probabment une ere à proté. l  laissé tber deuobjets, dirait un paremin et une pioche.)",
-                "Dialogue3": "3(Il semble que cet âne se soit enfui, il y a probabment une ere à proté. l  laissé tber deuobjets, dirait un paremin et une pioche.)",
-                "Dialogue4": "4(Il semble que cet âne se soit enfui, il y a probabment une ere à proté. l  laissé tber deuobjets, dirait un paremin et une pioche.)",
+                "Dialogue1": "l va falloir un peu de carburant si tu veux qu’on bouge de cette base.",
+                "Dialogue2": "T’as réussi à remplir la fusée ? Si oui on va pouvoir décoller.",
                 },
 
-
             "Alternatif": {
-                "Dialogue1": "(Cet âne n’a pas l’air de vouloir rentrer chez lui)"
+                "Dialogue1": "..."
             }
         },
         "PNJ6": {
-            "Nom" : "Âne fuyard",
+            "Nom" : "Han",
             "Principal": {
-                "Dialogue1": "fgsgdsfg1(Il semble que cet âne se soit enfui, il y a probabment une ere à proté. l  laissé tber deuobjets, dirait un paremin et une pioche.)",
-                "Dialogue2": "sdgsdfgsdfgsdfgsdfgsd2(Il semble que cet âne se soit enfui, il y a probabment une ere à proté. l  laissé tber deuobjets, dirait un paremin et une pioche.)",
-                "Dialogue3": "3dsfgsdfgsdfgdsfgsdfgsg(Il semble que cet âne se soit enfui, il y a probabment une ere à proté. l  laissé tber deuobjets, dirait un paremin et une pioche.)",
+                "Dialogue1": "Tu veux aller à la poursuite d’un roi fou qui veut conquérir toutes les dimensions ?!",
+                "Dialogue2": "Ça à l’air sympa, tant que je peux garder les outils.",
+                "Dialogue3": "Attends qu’est-ce qui se passe ? On a dû heurter quelque chose je n’ai plus les contrôles du vaisseau on va se crasher. ",
                 },
 
-
             "Alternatif": {
-                "Dialogue1": "(Cet âne n’a pas l’air de vouloir rentrer chez lui)"
-            }
+                "Dialogue1": "..."
+            },
         },
         "PNJ7": {
-            "Nom" : "Âne fuyard",
+            "Nom" : "Han",
             "Principal": {
-                "Dialogue1": "aaaaaaaaaaaaaaaaaaaaaaaaaa1(Il semble que cet âne se soit enfui, il y a probabment une ere à proté. l  laissé tber deuobjets, dirait un paremin et une pioche.)",
-                "Dialogue2": "bbbbbbbbbbbbbbbbbbbbbbbb2(Il semble que cet âne se soit enfui, il y a probabment une ere à proté. l  laissé tber deuobjets, dirait un paremin et une pioche.)",
-                "Dialogue3": "cccccccccccccccccccccc3(Il semble que cet âne se soit enfui, il y a probabment une ere à proté. l  laissé tber deuobjets, dirait un paremin et une pioche.)",
+                "Dialogue1": "(Le pilote semble assommé vous le laisser pour explorez les environ mais il dispose d’un carnet avec des inscriptions, vous le récupérez)",
                 },
 
-
             "Alternatif": {
-                "Dialogue1": "(Cet âne n’a pas l’air de vouloir rentrer chez lui)"
+                "Dialogue1": "(Le pilote semble assommé vous le laisser pour explorez les environ mais il dispose d’un carnet avec des inscriptions, vous le récupérez)"
             }
         },
     },
     "NiveauMordor" : {
         "PNJ1": {
-            "Nom" : "Bûcheron",
+            "Nom" : "Garde frontière orc",
             "Principal": {
-                "Dialogue1": "Bonjour toué ! Dis donc tu m’as l’air de frapper ton waterloo toi ! Mais lâche pas la patate je vais t’aider, cet arbre va pas te bloquer le chemin longtemps tabarnak !",
-                "Dialogue2": "Aussi prends c’te babiole ça pourrait t’aider t’sais, il y a écrit un charabia dessus j’te la laisse.",
+                "Dialogue1": "Un humain ?! Tu vas venir avec moi je t’offrirai à notre nouveau seigneur !",
+                "Dialogue2": "(Au moment de vous assommer, l’orc fait tomber un papier que vous ramassez)",
             },
             "Alternatif": {
-                "Dialogue1": "Alors, fonce l’ami n’ai pas peur de l’eau sous le tronc elle te sautera pas dessus !"
+                "Dialogue1": "..."
             }
         },
         "PNJ2": {
-            "Nom" : "Voyageur curieux",
+            "Nom" : "Prisonnière humaine",
             "Principal": {
-                "Dialogue1": "Salut l’ami si tu souhaites traverser l’Anduin il te faudra créer un pont je n’ai rien d’autre que ces planches alors je vais te les donner et j'espère que tu en feras bon usage.",
-                "Dialogue2": "J’ai même un manuel mais je ne sais pas s’il te sera utile.",
+                "Dialogue1": "Merci d’avoir ouvert cette cellule, je vais t’accompagner pour qu’on s’évade de cette maudite prison !",
+                "Dialogue2": "Tiens c’est un papier que j’ai trouvé dans mon pot de chambre mais je compte pas franchement le garder alors prends le.",
             },
             "Alternatif": {
-                "Dialogue1": "Alors que trouve-t-on de l’autre côté ? Tu me raconteras une fois que tu l’auras vu."
+                "Dialogue1": "Échappons- nous vite de cet horrible endroit !"
             }
         },
         "PNJ3": {
-            "Nom" : "Âne fuyard",
+            "Nom" : "Geôlier orc",
             "Principal": {
-                "Dialogue1": "(Il semble que cet âne se soit enfui, il y a probablement une ferme à proximité. Il a laissé tomber deux objets, on dirait un parchemin et une pioche.)",
-                "Dialogue2": "2(Il semblee cet âne se soit enui, il y a probablement une ferme à proximité. Il a laissé tomber deux objets, on dirait un parchemin et une pioche.)",
-                "Dialogue3": "3(Il semble que cet âne se s enfui, il y a probablement une fme à proximité. Il a laissé toer deux objets, on dirait un parchemin et une pioche.)",
-                "Dialogue4": "4(Il semble que cet  se soit enfui, il y a probablement une ferme à proximité. Il a laissé tomber deux objets, on diraitn parchemin et une pioche.)",
+                "Dialogue1": "(Vous ne pourrez surement pas le battre pour passer, trouvez un compagnon ou un autre moyen de vous évader.)",
+                "Dialogue2": "(Vous et votre compagnon affrontez votre geôlier, pendant le combat il fait tomber un parchemin que vous ramassez, vous pouvez maintenant passer.)",
             },
             "Alternatif": {
-                "Dialogue1": "(Cet âne n’a pas l’air de vouloir rentrer chez lui)"
+                "Dialogue1": "..."
             }
         },
         "PNJ4": {
-            "Nom" : "Âne fuyard",
+            "Nom" : "Goulboum",
             "Principal": {
-                "Dialogue1": "1(Il semble que cet âne se soit enfui, il y a probabment une ere à proté. l  laissé tber deuobjets, dirait un paremin et une pioche.)",
-                "Dialogue2": "2(Il semble que cet âne se soit enfui, il y a probabment une ere à proté. l  laissé tber deuobjets, dirait un paremin et une pioche.)",
-                "Dialogue3": "3(Il semble que cet âne se soit enfui, il y a probabment une ere à proté. l  laissé tber deuobjets, dirait un paremin et une pioche.)",
-                "Dialogue4": "4(Il semble que cet âne se soit enfui, il y a probabment une ere à proté. l  laissé tber deuobjets, dirait un paremin et une pioche.)",
+                "Dialogue1": "Mais que voit-on mon trésor, un humain tout frais ici ! Qu’à-t-il dit mon tréssssor ? Il souhaite retrouver le seigneur de ces terres ?!",
+                "Dialogue2": "Oh oui nous avons vu mon trésor, il est rentré dans la montagne cracheuse de feu, oh oui mon trésor !",
                 },
 
+            "Alternatif": {
+                "Dialogue1": "L’humain est revenu mon trésor, veut-il que nous le mangions ? Oh oui mon trésor !",
+                "Dialogue1": "De l’humain tout frais, ça fait si longtemps que nous nous contentons de poissons fort peu goûtus. Comment ça ?",
+                "Dialogue1": "Ça s'est juste trompé de chemin et ça ne veut pas être mangé. Oh non ! Peut-être la prochaine fois mon trésor.",
+            },
+        "PNJ5": {
+            "Nom" : "Roi",
+            "Principal": {
+                "Dialogue1": "Mais que voit-on mon trésor, un humain tout frais ici ! Qu’à-t-il dit mon tréssssor ? Il souhaite retrouver le seigneur de ces terres ?!",
+                "Dialogue2": "Oh oui nous avons vu mon trésor, il est rentré dans la montagne cracheuse de feu, oh oui mon trésor !",
+                },
 
             "Alternatif": {
-                "Dialogue1": "(Cet âne n’a pas l’air de vouloir rentrer chez lui)"
-            }
+                "Dialogue1": "AHAHAH ! Tu as déjà échoué une fois, tu crois pouvoir me vaincre cette fois ?!",
+
+                },
+            },
         },
     },
 }
