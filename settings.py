3021b131cc790e45b946b826a61033538c3a8d73
Gandalf <quentin.pladeau@gmail.com>
2025-03-25 23:19:47 +0100

    [/]

--- settings.py
+++ settings.py
@@ -164,7 +164,7 @@ STATE_HELP_INFOS = ["SeePNJ"] # list pour pouvoir etre modifié : tips
 
 NIVEAU = {
      # niveau
-    "Niveau" : "Premiere",
+    "Niveau" : "Seconde",
     "All" : False, 
 
     # map
